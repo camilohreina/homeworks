d0bb439e93a9ba0b7176232747371132216617d0
Camilo Hernandez <camilohreina.dev@gmail.com>
2024-04-15 22:15:43 -0500

    add search bar and data movies

--- movies-app/src/App.jsx
+++ movies-app/src/App.jsx
@@ -1,11 +1,9 @@
+import { Route } from 'wouter';
+import Home from './pages/Home';
 function App() {
   return (
     <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
-      <div className="container mx-auto">
-        <h1 className="text-3xl text-white font-bold underline">
-          Hello world!
-        </h1>
-      </div>
+      <Route path="/" component={Home} />
     </div>
   );
 }
---
+++ movies-app/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import SearchBar from '../components/SearchBar';
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-full w-full">
+      <SearchBar />
+    </div>
+  );
+}
