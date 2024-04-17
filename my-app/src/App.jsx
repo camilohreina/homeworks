f3f47f6f57a2b1bc584c0b442741314507e40bb3
Camilo Hernandez <camilohreina.dev@gmail.com>
2024-04-17 16:06:19 -0500

    add useCounter hook

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,7 +4,7 @@ import FirstApp from './components/FirstApp';
 function App() {
   return (
     <>
-      <FirstApp title="counter" sum={0} />
+      <FirstApp title="counter" sum={10} />
     </>
   );
 }
--- my-app/src/components/FirstApp.jsx
+++ my-app/src/components/FirstApp.jsx
@@ -1,29 +1,17 @@
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import { useCounter } from '../hooks/useCounter';
 
 const FirstApp = ({ title, sum }) => {
-  const [counter, setCounter] = useState(sum);
-
-  const handleAdd = () => {
-    setCounter(counter + 1);
-  };
-
-  const handleSubtract = () => {
-    setCounter(counter - 1);
-  };
-
-  const handleReset = () => {
-    setCounter(sum);
-  };
+  const { count, increment, decrement, reset } = useCounter(sum);
 
   return (
     <>
       <h1>{title}</h1>
-      <p>{counter}</p>
+      <p>{count}</p>
       <div style={{ display: 'flex', gap: '10px', justifyContent: 'center' }}>
-        <button onClick={() => handleAdd()}>+1</button>
-        <button onClick={() => handleSubtract()}>-1</button>
-        <button onClick={() => handleReset()}>-Reset</button>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+        <button onClick={reset}>-Reset</button>
       </div>
     </>
   );
